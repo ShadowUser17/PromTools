0876c812c363fef79b3f8111dd9d9f53393ca4bc
ShadowUser17 <linuxforce17@gmail.com>
2022-05-30 14:55:09 +0300

    Changes to be committed: 	modified:   get_aws_resources.py

--- prometheus/get_aws_resources.py
+++ prometheus/get_aws_resources.py
@@ -3,6 +3,7 @@ from urllib import parse as urllib
 from urllib import request
 
 import json
+import typing
 import argparse
 import traceback
 
@@ -10,10 +11,14 @@ import traceback
 def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', dest='target', default='http://127.0.0.1:9090', help='Set Prometheus address.')
+    parser.add_argument('-f', dest='filter', default='=', help='Set field of filtering. (key=value)')
     return parser.parse_args()
 
 
 def get_resources(target: str) -> list:
+    if not target.startswith('http'):
+        target = 'http://' + target
+
     url = urllib.urljoin(target, '/api/v1/query?query=aws_resource_info')
     req = request.Request(url, method='GET')
 
@@ -22,18 +27,23 @@ def get_resources(target: str) -> list:
         return json.loads(data.decode())
 
 
-def parse_resources(raw_data: list) -> list:
-    parsed_data = []
-
+def parse_resources(raw_data: list) -> typing.Generator:
     for item in iter(raw_data['data']['result']):
-        parsed_data.append(item['metric'])
-
-    return parsed_data
+        yield item['metric']
 
 
-def print_resources(parsed_data: list) -> None:
+def filter_resources(parsed_data: typing.Generator, field: str) -> typing.Generator:
+    (field_key, field_val) = field.split('=')
+
     for item in parsed_data:
-        for (key, val) in item.items():
+        if (item.get(field_key, '') == field_val):
+            for (key, val) in item.items():
+                yield (key, val)
+
+
+def print_resources(filtered_data: typing.Generator) -> None:
+    for (key, val) in filtered_data:
+        if key != '__name__':
             print('{}: {}'.format(key, val))
 
         else:
@@ -45,6 +55,7 @@ if __name__ == '__main__':
         args = get_args()
         data = get_resources(args.target)
         data = parse_resources(data)
+        data = filter_resources(data, args.filter)
         print_resources(data)
 
     except Exception:
